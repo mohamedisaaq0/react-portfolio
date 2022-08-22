dafd5a4d2fc6c33ec8068d88626a27da752be2a4
mohamed hassan <mohamedisaaq0@gmail.com>
2022-08-22 19:28:22 +0100

    installed dependecies for react app and created structure file

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Header() {
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1>
+              <span>P</span>ort<span>f</span>olio
+            </h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li>
+              <a href="#hero" data-after="Home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#projects" data-after="Projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#about" data-after="About">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#contact" data-after="Contact">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/mohamedisaaq0" data-after="Contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+}
