28f89bf5693a9a0b12a02187464e933bd6eb779b
mohamed hassan <mohamedisaaq0@gmail.com>
2022-08-28 01:41:32 +0100

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section id="footer">
+      <div class="footer container">
+        <div class="social-icon">
+          <div class="social-item">
+            <a href="https://github.com/mohamedisaaq0">
+              <img src="https://img.icons8.com/plasticine/100/000000/github-squared.png" />
+            </a>
+          </div>
+          <div class="social-item">
+            <a href="https://www.linkedin.com/in/mohamedisaaq0/">
+              <img src="https://img.icons8.com/color/48/000000/linkedin.png" />
+            </a>
+          </div>
+          <div class="social-item">
+            <a href="mailto: mohamedisaaq0@gmail.com">
+              <img src="https://img.icons8.com/color-glass/48/000000/gmail.png" />
+            </a>
+          </div>
+        </div>
+        <p>Copyright © 2022 Mohamed Hassan. All rights reserved</p>
+      </div>
+    </section>
+  );
+}
