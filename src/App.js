c3b01ce1befa7abfea9c759a854bbd02bb45d96e
mohamed hassan <mohamedisaaq0@gmail.com>
2022-08-28 01:42:04 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Hero />
+      <AboutMe />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
   );
 }
